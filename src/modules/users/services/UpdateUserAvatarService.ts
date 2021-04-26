5816d22c90c8bbf969fdfee82e62043b070b7e54
Wesley <wesleybpereira@gmail.com>
2021-04-26 20:27:19 -0300

    Testes e TDD

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,6 @@
-import path from 'path';
-import fs from 'fs';
-
-import uploadConfig from '@config/upload';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
